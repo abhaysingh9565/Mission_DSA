ba22d3112e8e297633762c5090a771775e8d2ca4
Abhay Singh <abhaysingh@Abhays-MacBook-Air.local>
2026-08-02 06:53:59 +0530

    new question added

---
+++ leetcode/1004_Max_Consecutive_Ones_III.cpp
@@ -0,0 +1,79 @@
+class Solution {
+  public:
+      int longestOnes(vector<int>& nums, int k) {
+          int ans = 0 ; 
+          for(int j = 0 ,i = 0 ; i<nums.size() ; i++)
+          {
+              if(nums[i]==0)
+              {
+                  if(!k)
+                  {
+                      while(nums[j]!=0)j++;
+  
+                      j++;
+                  }
+                  else 
+                  k--;
+              }
+              ans = max(ans,i-j+1);
+          }
+          return ans;
+          
+      }
+  };
+
+
+//o(n^2) solution
+
+class Solution1 {
+  public:
+      int longestOnes(vector<int>& nums, int k) {
+          int ans = 0 ; 
+          for(int j = 0 ,i = 0 ; i<nums.size() ; i++)
+          {
+              if(nums[i]==0)
+              {
+                  if(!k)
+                  {
+                      while(nums[j]!=0)j++;
+  
+                      j++;
+                  }
+                  else 
+                  k--;
+              }
+              ans = max(ans,i-j+1);
+          }
+          return ans;
+          
+      }
+  };
+  
+  class Solution {
+  public:
+      int longestOnes(vector<int>& nums, int k) {
+          int ans = 0 ; 
+          for(int i = 0 ; i<nums.size() ; i++)
+          {
+              int flip = k;
+              int j = i;
+              while(j<nums.size())
+              {
+                  if(nums[j]==0){
+                      if(!flip){
+                          break;
+                      }
+                      else flip--;
+                  }
+  
+                  j++;
+              }
+              if(j==nums.size())
+              return max(ans,j-i);
+  
+              ans = max(ans,j-i);
+          }
+          return ans;
+          
+      }
+  };
