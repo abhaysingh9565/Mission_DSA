e7539652a596dcc7dbaeb9209b2e144c0538992f
Abhay Singh <abhaysingh@Abhays-MacBook-Air.local>
2026-08-05 11:27:52 +0530

    new question added

---
+++ leetcode/239_Sliding_Window_Maximum.cpp
@@ -0,0 +1,74 @@
+// brute force
+class Solution {
+  public:
+      vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+          int n = nums.size();
+          vector<int>result;
+          for(int i = 0; i<=n-k; i++)
+          {
+              int ans = nums[i];
+              for(int j = i ;j < i+k ;j++)
+              {
+                  ans = max(ans,nums[j]);
+              }
+              result.push_back(ans);
+              
+          }
+          return result;
+          
+      }
+  };
+
+  // using prority queue
+
+  class Solution {
+    public:
+        vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+            priority_queue<pair<int,int>> pq;
+        vector<int> ans;
+    
+        for(int i=0;i<nums.size();i++){
+    
+            pq.push({nums[i],i});
+    
+            while(!pq.empty() && pq.top().second<=i-k){
+                pq.pop();
+            }
+    
+            if(i>=k-1){
+                ans.push_back(pq.top().first);
+            }
+        }
+    
+        return ans;
+            
+        }
+    };
+
+    // using deque
+    class Solution {
+      public:
+          vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+              deque<int> dq;
+              vector<int> ans;
+      
+              for(int i=0;i<nums.size();i++){
+      
+      
+                  while(!dq.empty() && dq.front()<=i-k)
+                      dq.pop_front();
+      
+      
+                  while(!dq.empty() && nums[dq.back()]<nums[i])
+                      dq.pop_back();
+      
+                  dq.push_back(i);
+      
+                  if(i>=k-1)
+                      ans.push_back(nums[dq.front()]);
+              }
+      
+              return ans;
+              
+          }
+      };
