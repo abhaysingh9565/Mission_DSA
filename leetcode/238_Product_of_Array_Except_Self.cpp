f876bba4b74e61197509f88a78ae0173a8cc1b82
Abhay Singh <abhaysingh@Abhays-MacBook-Air.local>
2026-08-07 12:06:26 +0530

    new question added

---
+++ leetcode/238_Product_of_Array_Except_Self.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+      vector<int> productExceptSelf(vector<int>& nums) {
+          int n = nums.size();
+          vector<int>ans(n,0);
+          int total = 1;
+          int flag = 0 ;
+          int index;
+          for(int i = 0; i< n ; i++)
+          {
+              if(nums[i]==0)
+              {
+                  flag++;
+                  index = i;
+                  if(flag>1)return ans;
+              }
+              else total *= nums[i];
+          }
+          if(flag == 1)
+          {
+              ans[index]=total;
+          }
+          else {
+              for(int i = 0 ; i< n ; i++)
+              {
+                  ans[i] = total/nums[i];
+              }
+          }
+          return ans;
+          
+      }
+  };
