63c8004e190b1831d8c278e7e1bdeaa7fce96b2f
Abhay Singh <abhaysingh@Abhays-MacBook-Air.local>
2026-08-09 15:40:28 +0530

    new question added

---
+++ leetcode/3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+      int lengthOfLongestSubstring(string s) {
+          vector<int>freq(256,0);
+          int i = 0 ,ans =0;
+          for(int j = 0 ; j<s.size();j++)
+          {
+              freq[s[j]]++;
+              if(freq[s[j]]>1)
+              {
+                  while(s[i]!=s[j]){
+                      freq[s[i]]--;
+                      i++;
+                  }
+                  if(i<s.size()){
+                      freq[s[i]]--;
+                      i++;
+                  }
+              }
+              ans = max(ans,j-i+1);
+          }
+          return ans;
+  
+          
+      }
+  };
---
+++ leetcode/438_Find_All_Anagrams_in_a_String.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+      vector<int> findAnagrams(string s, string p) {
+          if(s.size()<p.size())return {};
+          vector<int>freq(26,0);
+          vector<int>freq2(26,0);
+          for(int i =0 ; i< p.size();i++)
+          {
+              freq[p[i]-'a']++;
+              freq2[s[i]-'a']++;
+          }
+          vector<int>ans;
+          for(int j = p.size();j<s.size();j++)
+          {
+              if(freq==freq2)
+              {
+                  ans.push_back(j-p.size());
+              }
+              freq2[s[j]-'a']++;
+              freq2[s[j-p.size()]-'a']--;
+          }
+          if(freq==freq2)
+              {
+                  ans.push_back(s.size()-p.size());
+              }
+          return ans;
+          
+      }
+  };
