c487800342003ef3df33af38e9028fe72fd1f0b6
Abhay Singh <abhaysingh@Abhays-MacBook-Air.local>
2026-08-10 09:18:40 +0530

    new question added

---
+++ leetcode/1047_Remove_All_Adjacent_Duplicates_In_String.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+      string removeDuplicates(string s) {
+          stack<char>st;
+          string ans = "";
+          bool flag = false;
+          for(int i  = 0 ; i< s.size() ; i++)
+          {
+              while(!st.empty() && st.top()==s[i]){
+                  st.pop();
+                  flag = true;
+              }
+              if(flag){
+                  flag = false;
+                  continue;
+              }
+              st.push(s[i]);
+          }
+          while(!st.empty())
+          {
+              ans += st.top();
+              st.pop();
+          }
+          reverse(ans.begin(),ans.end());
+          return ans;
+          
+      }
+  };
