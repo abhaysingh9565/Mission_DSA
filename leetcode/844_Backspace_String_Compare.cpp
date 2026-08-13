3fba842db8e7a9c905bba7527d2e2439dc0be4ef
Abhay Singh <2k24.cs1a.2414293@gmail.com>
2026-08-13 16:39:29 +0530

    new question added

---
+++ leetcode/844_Backspace_String_Compare.cpp
@@ -0,0 +1,45 @@
+class Solution {
+  public:
+      void format(string &s)
+      {
+          for(int i = 0 ; i < s.size() ; i++)
+          {
+              if(s[i]=='#')
+              {
+                  int j = i-1 ;
+                  while(j>=0 && s[j]=='#')j--;
+  
+                  if(j>=0)s[j]='#';
+  
+              }
+          }
+      }
+      bool backspaceCompare(string s, string t) {
+          format(s);
+          format(t);
+          int i = 0 , j = 0;
+          while(i<s.size() && j < t.size())
+          {
+              if(s[i]=='#')i++;
+              else if(t[j]=='#')j++;
+              else if(s[i]!=t[j])return false;
+  
+              else{
+                  i++;
+                  j++;
+              }
+          }
+          while(i< s.size())
+          {
+              if(s[i++]!='#')return false;
+          }
+          while(j< t.size())
+          {
+              if(t[j++]!='#')return false;
+          }
+          return true;
+          
+      }
+  };
+
+  
