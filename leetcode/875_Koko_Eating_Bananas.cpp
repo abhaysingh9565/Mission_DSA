baf65dec5224796c1fab872000d1e248d30a2fb2
Abhay Singh <2k24.cs1a.2414293@gmail.com>
2026-08-13 09:19:59 +0530

    new question added

---
+++ leetcode/875_Koko_Eating_Bananas.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+      bool canEat(vector<int>& piles , int mid , int h)
+      {
+          int hours = 0 ;
+          for(int num : piles)
+          {
+              hours += num/mid;
+              if(num%mid != 0){
+                  hours ++;
+              }
+          }
+          return hours<=h;
+      }
+      int minEatingSpeed(vector<int>& piles, int h) {
+          int s = 1 ;
+          int e = *max_element(piles.begin(),piles.end());
+          while(s<e)
+          {
+              int mid = s + ( e-s)/2;
+              if(canEat(piles,mid,h))
+              {
+                  e = mid;
+              }
+              else {
+                  s = mid+1;
+              }
+          }
+          return s;
+  
+          
+      }
+  };
