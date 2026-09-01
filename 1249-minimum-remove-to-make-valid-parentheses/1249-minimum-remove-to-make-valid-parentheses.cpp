d74d7a915bb96d96986019e38f6b0ff9cb39fb5d
abhaysingh9565 <2k24.cs1a.2414293@gmail.com>
2026-09-01 10:00:11 +0530

    Time: 21 ms (6.55%), Space: 15.4 MB (14.96%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,33 +1,44 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
+        string ans  = "";
+        stack<int>extra;
         stack<int>st;
         for(int i = 0 ; i < s.size() ;i++)
         {
             if(s[i]=='(')
             {
                 st.push(i);
+                ans+=s[i];
             }
             else if(s[i]==')')
             {
                 if(!st.empty())
                 {
                     st.pop();
-                }else s[i]='#';
+                    ans+=s[i];
+                }else extra.push(i);
             }
-        }
-        while(!st.empty())
-        {
-            s[st.top()]='#';
-            st.pop();
+            else
+                ans+=s[i];
         }
         string result = "";
-        for(char c : s)
+        if(st.empty())return ans;
+        for(int i = s.size()-1; i>=0  ; i--)
         {
-            if(c !='#'){
-                result +=c;
+            if(!st.empty() && i == st.top())
+            {
+                st.pop();
+                continue;
             }
+            else if(!extra.empty() && i == extra.top())
+            {
+                extra.pop();
+                continue;
+            }
+            result+=s[i];
         }
+        reverse(result.begin(),result.end());
         return result;
     }
 };
