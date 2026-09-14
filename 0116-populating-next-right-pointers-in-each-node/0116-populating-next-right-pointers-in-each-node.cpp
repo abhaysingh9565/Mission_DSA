540b07b02139059e29b0c22710e287b37514b827
abhaysingh9565 <2k24.cs1a.2414293@gmail.com>
2026-09-14 09:01:00 +0530

    Time: 12 ms (69.64%), Space: 19.4 MB (8.87%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -27,24 +27,13 @@ public:
         while(!q.empty())
         {
             int n = q.size();
-            int i = 0 ;
-            Node* prev = nullptr;
-            Node* temp = nullptr;
-            while(n--)
+            for(int i = 0 ; i< n; i++)
             {
-                temp = q.front();
+                Node* temp = q.front();
                 q.pop();
-                if(prev)
-                {
-                    prev->next = temp;
-                }
-                prev = temp;
+                temp->next = i+1==n?nullptr:q.front();
                 if(temp->left)q.push(temp->left);
                 if(temp->right)q.push(temp->right);
-            }
-            if(prev)
-            {
-                prev->next = nullptr;
             }
 
         }
