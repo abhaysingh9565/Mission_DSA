e0fe5dd65ff742f45fe2e1b86212a15e1e4827cd
abhaysingh9565 <2k24.cs1a.2414293@gmail.com>
2026-09-24 19:48:15 +0530

    Time: 0 ms (100%), Space: 25.6 MB (12.87%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -5,15 +5,10 @@ public:
         while(s<e)
         {
             int sum = numbers[s]+numbers[e];
-            if(sum == target){
-                return {s+1,e+1};
-            }
-            else if(sum < target)
-            {
-                s++;
-            }
+            if(sum==target)return {s+1,e+1};
+            else if(sum < target)s++;
             else e--;
         }
-        return {-1,-1};
+        return {};
     }
 };
